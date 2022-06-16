c4397db2297ad5faa6161dd3c905067f6fbb0c40
Mathieu <97654258+Mathieuryr@users.noreply.github.com>
2022-06-16 20:53:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnSucess = document.querySelector(".btn-success");
+
+const cookies = document.querySelector(".cookies");
+
+cookies.addEventListener("click", function () {
+  cookies.style.opacity = "0";
+  cookies.style.transition = "0.8s";
+});
